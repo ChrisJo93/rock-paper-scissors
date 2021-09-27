698ce29eee454081914353e259267398aedec782
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-09-26 20:35:11 -0500

    switch case logic made

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { rock, paper, scissors } from './assets/data';
 import './game.css';
 
@@ -10,13 +10,68 @@ export default function Game() {
     opponentScore: 0,
   });
 
+  useEffect(() => {
+    console.log(state.opponent);
+  });
+
   const findWinner = () => {
     // compare hands using switch statements to handle ties or greater than situations.
     //run new game function
+    let player = state.player;
+    let opp = state.opponent;
+
+    switch (true) {
+      //paper cases
+      case player === 'paper' && opp === 'scissors':
+        alert('You lose');
+        newGame();
+        break;
+      case player === 'paper' && opp === 'paper':
+        alert('Tied');
+        newGame();
+        break;
+      case player === 'paper' && opp === 'rock':
+        alert('You win!');
+        newGame();
+        break;
+      //scissor cases
+      case player === 'scissors' && opp === 'paper':
+        alert('You win');
+        newGame();
+        break;
+      case player === 'scissors' && opp === 'scissors':
+        alert('Tied');
+        newGame();
+        break;
+      case player === 'scissors' && opp === 'rock':
+        alert('You lose');
+        newGame();
+        break;
+      //rock cases
+      case player === 'rock' && opp === 'scissors':
+        alert('You win');
+        newGame();
+        break;
+      case player === 'rock' && opp === 'rock':
+        alert('Tied');
+        newGame();
+        break;
+      case player === 'rock' && opp === 'paper':
+        alert('You lose');
+        newGame();
+        break;
+      default:
+        return null;
+    }
+  };
+
+  const randomHand = () => {
+    return Math.floor(Math.random() * 3);
   };
 
   const opponentPick = (hand) => {
-    console.log(hand);
+    const choices = ['scissors', 'rock', 'paper'];
+    setState({ ...state, opponent: choices[randomHand()] });
   };
 
   const handlePickEvent = (e, hand) => {
